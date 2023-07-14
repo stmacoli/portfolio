f7885feb4851acc7dca6ebb930be4bea435aa5c1
StMacoli <st.macoli@gmail.com>
2023-07-14 01:38:31 -0300

    calculadora-photo-change

--- js/content.js
+++ js/content.js
@@ -1,16 +1,28 @@
+const sectionOnDisplay = document.getElementById("navbar-content");
+const projetosDiv = document.getElementById("projetos-container");
+const aboutDiv = document.getElementById("about-container");
+
+
+
 function showContent(target) {
-    const sectionOnDisplay = document.getElementById("navbar-content");
-    const projetosDiv = document.getElementById("projetos-container");
-    const aboutDiv = document.getElementById("about-container");
 
     //const calculatorDiv = document.getElementById("calculator")
     //, 'about-container', 'arts-container', 'cv-container'
   
     if (target === "projetos") {
-      sectionOnDisplay.classList.add('show')
-    
-      projetosDiv.style.borderRadius = '10px'
-      projetosDiv.classList.add('show')
+
+
+
+
+
+      
+      
+        sectionOnDisplay.classList.add('show')
+        
+        projetosDiv.style.borderRadius = '10px'
+        projetosDiv.classList.add('show')
+
+      
     } else if (target === "about-container") { 
       sectionOnDisplay.classList.add('show')
       sectionOnDisplay.style.display = "block";
@@ -28,12 +40,26 @@ function showContent(target) {
     } //else if (target === "calculator") {
      // calculatorDiv.style.backgroundColor = "blue";
    // }
+
+
+ 
   }
+
+  function hideContent (target) {
+    if (target === "projetos" && projetosDiv.classList.contains('show')) {
+      sectionOnDisplay.classList.remove('show');
+      return;
+    }
+
+  }
+  
+
   
 
 
 
   let overlayDiv = document.getElementById("calculator");
+
   document.getElementById("open-overlay").addEventListener("click", function() {
     overlayDiv.style.display = "block";
   });
