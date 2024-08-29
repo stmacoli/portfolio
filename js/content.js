47e3449023ed3d2cb1202f0a32f397f7659a1b14
StMacoli <st.macoli@gmail.com>
2024-08-29 01:16:44 -0300

    new_navbar

--- js/content.js
+++ js/content.js
@@ -4,17 +4,81 @@ const aboutDiv = document.getElementById("about-container");
 
 
 
-function showContent(target) {
+// Seleciona o botão do dropdown
+const dropdownBtn = document.querySelector('.dropdown-btn1');
+// Seleciona o conteúdo do dropdown
+const dropdownContent = document.querySelector('.dropdown-content1');
 
-    //const calculatorDiv = document.getElementById("calculator")
-    //, 'about-container', 'arts-container', 'cv-container'
+// Adiciona um evento de clique ao botão
+dropdownBtn.addEventListener('click', function() {
+    // Alterna entre mostrar e esconder o conteúdo do dropdown
+    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+});
+
+// Fecha o dropdown se o usuário clicar fora dele
+window.addEventListener('click', function(event) {
+    if (!event.target.matches('.dropdown-btn1')) {
+        dropdownContent.style.display = 'none';
+    }
+});
+
+
+
+
+
+// Seleciona o botão do dropdown
+const dropdownBtn2 = document.querySelector('.dropdown-btn2');
+// Seleciona o conteúdo do dropdown
+const dropdownContent2 = document.querySelector('.dropdown-content2');
+
+// Adiciona um evento de clique ao botão
+dropdownBtn2.addEventListener('click', function() {
+    // Alterna entre mostrar e esconder o conteúdo do dropdown
+    dropdownContent2.style.display = dropdownContent2.style.display === 'block' ? 'none' : 'block';
+});
+
+// Fecha o dropdown se o usuário clicar fora dele
+window.addEventListener('click', function(event) {
+    if (!event.target.matches('.dropdown-btn2')) {
+        dropdownContent2.style.display = 'none';
+    }
+});
+
+
+
+// Seleciona o botão do dropdown
+const dropdownBtn3 = document.querySelector('.dropdown-btn3');
+// Seleciona o conteúdo do dropdown
+const dropdownContent3 = document.querySelector('.dropdown-content3');
+
+// Adiciona um evento de clique ao botão
+dropdownBtn3.addEventListener('click', function() {
+    // Alterna entre mostrar e esconder o conteúdo do dropdown
+    dropdownContent3.style.display = dropdownContent3.style.display === 'block' ? 'none' : 'block';
+});
+
+// Fecha o dropdown se o usuário clicar fora dele
+window.addEventListener('click', function(event) {
+    if (!event.target.matches('.dropdown-btn3')) {
+        dropdownContent3.style.display = 'none';
+    }
+});
+
+
+
+
+
+function showContent(target) {
+   
+    
   
     if (target === "projetos") {
+        
+      
 
 
 
-
-
+        
       
       
         sectionOnDisplay.classList.add('show')
